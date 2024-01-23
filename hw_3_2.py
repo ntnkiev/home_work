a408953aa7713478d417c4bb9863a2d5a131b63e
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-23 18:06:16 +0200

    import sample

--- hw_3_2.py
+++ hw_3_2.py
@@ -1,19 +1,10 @@
-import random
+from random import sample
 
 
 def get_numbers_ticket(min, max, quantity):
-    if (
-        1 <= min < 1000 and 1 <= max <= 1000 and min < max and quantity < max - min
-    ):  # перевірка вхідних даних
-        numbers = random.sample(
-            range(min, max + 1), quantity
-        )  # генерація випадкової послідовності
+    if  1 <= min < 1000 and 1 <= max <= 1000 and min < max and quantity < max - min:  # перевірка вхідних даних
+        numbers = sample(range(min, max + 1), quantity)  # генерація випадкової послідовності
         numbers.sort()  # сортування
         return numbers
     else:
         return []
-
-
-print(get_numbers_ticket(1, 36, 5))
-print(get_numbers_ticket(1, 49, 6))
-print(get_numbers_ticket(1, 1, 1))
