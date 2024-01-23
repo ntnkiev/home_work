8ae88ba240288353fa8e5e0e1d65301383c8be8d
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-23 23:04:40 +0200

    comments

--- hw_3_3.py
+++ hw_3_3.py
@@ -3,11 +3,10 @@ def normalize_phone(num):
     num = re.findall(r"\d+", num)
     num = ''.join(num)
     if len(num) == 10:
-        result = '+38' + num
+        return '+38' + num
         return result
     elif len(num) == 12:
-        result = '+' + num
-        return result
+        return '+' + num
     else:
         return None
 
