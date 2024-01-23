7a633b6474f720846723d86738ce8fae01403ce3
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-23 16:52:06 +0200

    format file

--- hw_3_1.py
+++ hw_3_1.py
@@ -1,9 +1,12 @@
 import datetime
+
+
 def get_days_from_today(date):
     try:
-        date_from = datetime.datetime.strptime(date, "%Y-%m-%d") #перевод в фотрат datetime
+        date_from = datetime.datetime.strptime(
+            date, "%Y-%m-%d"
+        )  # перевод в фотрат datetime
     except ValueError:
-        return None                                              #якщо невірний формат повертаєм None   
-    date_now = datetime.datetime.today()                         #сьогоднішня дата
-    return date_now.toordinal() - date_from.toordinal()          #повертаємо різницю днів
-    
+        return None  # якщо невірний формат повертаєм None
+    date_now = datetime.datetime.today()  # сьогоднішня дата
+    return date_now.toordinal() - date_from.toordinal()  # повертаємо різницю днів
