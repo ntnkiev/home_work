edad205b1ca9689d6f7f7c998aeea9480db63d94
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-24 13:48:06 +0200

    date

--- hw_3_3.py
+++ hw_3_3.py
@@ -1,12 +1,10 @@
 import re
 def normalize_phone(num):
-    num = re.findall(r"\d+", num)
-    num = ''.join(num)
+    num = re.findall(r"\d+", num) #виділення чисел
+    num = ''.join(num) #поєднання в один рядок
     if len(num) == 10:
-        return '+38' + num
-        return result
+        return '+38' + num #якщо не вистачає коду держави, додаємо '+38'
     elif len(num) == 12:
-        return '+' + num
+        return '+' + num #якщо не вистачає лише '+', додаємо '+'
     else:
-        return None
-
+        return None #якщо номер не підпадає під умови 
