df1a7eab885393fcc6fdecda27e91f77f9b85058
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-25 17:01:12 +0200

    beta

--- hw_3_4.py
+++ hw_3_4.py
@@ -1,29 +1,26 @@
 import datetime as dt
 from datetime import datetime as dtdt
 def get_upcoming_birthdays(users):
-    congratulation_dict = {} #словник вітань
+    congratulation_list = [] #словник вітань
     current_date = dtdt.today().date() #поточна дата
     for user in users:
         bday = dtdt.strptime(user["birthday"], "%Y.%m.%d").date() #день народження
         bday = bday.replace(year = current_date.year) #зміна року народження на поточний
-        if bday < current_date: #якщо дн вже пройшов, додаємо 1 рік
+        if bday.toordinal() < current_date.toordinal(): #якщо дн вже пройшов, додаємо 1 рік
             bday = bday.replace(year = current_date.year + 1)
-            if bday.toordinal() - current_date.toordinal() < 7: #якщо різниця менше 7 днів:
-                if bday.weekday() == 5:
+        if bday.toordinal() - current_date.toordinal() < 7: #якщо різниця менше 7 днів:
+            if bday.weekday() == 5: #якщо день народження випадає на суботу, додається 2 дні
                     bday = bday + dt.timedelta(days=2)
-                elif bday.weekday() == 6:
+            elif bday.weekday() == 6: #якщо день народження випадає на неділю, додається 1 день
                     bday = bday + dt.timedelta(days=1)
-                else:
-                    pass
-            pass
-        pass
-    pass                    
+            congratulation_list.append({'name': user["name"], 'congratulation_day': bday.strftime("%Y.%m.%d")}) #додавання іменинника до списку
+    return congratulation_list                    
 
 
 users = [
     {"name": "John Doe", "birthday": "1985.01.23"},
     {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Anthon Asieiev", "birthday": "1972.01.27"}
+    {"name": "Anthon Asieiev", "birthday": "1972.01.30"}
 ]
 
-get_upcoming_birthdays(users)
+print(get_upcoming_birthdays(users))
