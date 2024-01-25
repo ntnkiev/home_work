61dd69459a45fcab4caa7076451ef94249ea5158
Anton Asieiev <ntn.kiev@gmail.com>
2024-01-25 17:27:45 +0200

    comments

--- hw_3_4.py
+++ hw_3_4.py
@@ -6,9 +6,9 @@ def get_upcoming_birthdays(users):
     for user in users:
         bday = dtdt.strptime(user["birthday"], "%Y.%m.%d").date() #день народження
         bday = bday.replace(year = current_date.year) #зміна року народження на поточний
-        if bday.toordinal() < current_date.toordinal(): #якщо дн вже пройшов, додаємо 1 рік
+        if bday.toordinal() < current_date.toordinal(): #якщо дн вже пройшов, додається 1 рік
             bday = bday.replace(year = current_date.year + 1)
-        if bday.toordinal() - current_date.toordinal() < 7: #якщо різниця менше 7 днів:
+        if bday.toordinal() - current_date.toordinal() <= 7: #якщо різниця менше 7 днів:
             if bday.weekday() == 5: #якщо день народження випадає на суботу, додається 2 дні
                     bday = bday + dt.timedelta(days=2)
             elif bday.weekday() == 6: #якщо день народження випадає на неділю, додається 1 день
@@ -20,7 +20,7 @@ def get_upcoming_birthdays(users):
 users = [
     {"name": "John Doe", "birthday": "1985.01.23"},
     {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Anthon Asieiev", "birthday": "1972.01.30"}
+    {"name": "Anthon Asieiev", "birthday": "1972.02.02"}
 ]
 
 print(get_upcoming_birthdays(users))
